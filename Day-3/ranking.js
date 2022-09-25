42dac5599ba84098e14193646b716857167ac9f9
Rahmath-001 <rehamathullah7@gmail.com>
2022-09-25 20:50:27 +0530

    Button hover changes

--- Day-3/ranking.js
+++ Day-3/ranking.js
@@ -1,31 +1,116 @@
+let winnerData = [
+    [
+        { playerName: "Elon", attempts: 4 },
+        { playerName: "Vishnu", attempts: 4 },
+        { playerName: "Vishwesh", attempts: 4 }
+    ],
+    [
+        { playerName: "Prash", attempts: 3 },
+        { playerName: "Saad", attempts: 3 },
+        { playerName: "Adnan", attempts: 3 },
+        { playerName: "Musk", attempts: 3 }
+    ],
+    [
+        { playerName: "Cenfu", attempts: 1 },
+    ],
+    [
+        { playerName: "abc", attempts: 2 },
+        { playerName: "kpi", attempts: 2 },
+        { playerName: "qwerty", attempts: 2 },
+        { playerName: "poi", attempts: 2 },
+    ],
+    [
+        { playerName: "sd", attempts: 8 },
+        { playerName: "sdf", attempts: 8 },
+        { playerName: "qwesdfrty", attempts: 8 },
+        { playerName: "sdfsdf", attempts: 8 },
+    ],
+    [
+        { playerName: "Huda", attempts: 5 },
+        { playerName: "Rahmath", attempts: 5 }
+    ],
+    [
+        { playerName: "Majid", attempts: 6 }
+    ],
+    [
+        { playerName: "Ateeq", attempts: 1 }
+    ]
+];
 
 
 
-function newRankings(winnerData, newWinner) {
 
-    const newWinners = [];
-    let temp = [];
-    winnerData = winnerData.flat();
-    winnerData.push(newWinner);
-    winnerData.sort((a, b) => a.attempts - b.attempts);
+let newWinners = [];
+let temp = [];
 
-    temp[0] = winnerData[0];
-        
-    localStorage.getItem(username)
-    
-    for (let i = 1; i < winnerData.length; i++) {
-        if (winnerData[i].attempts === winnerData[i - 1].attempts) {
-            temp.push(winnerData[i]);
-        } else {
-            newWinners.push(temp);
-            temp = [];
-            temp.push(winnerData[i]);
-        }
+
+// console.log(winnerData);//2D Array
+winnerData = winnerData.flat();
+winnerData.sort((a, b) => a.attempts - b.attempts);
+temp[0] = winnerData[0];
+// console.log(winnerData);
+let i;
+for (i = 1; i < winnerData.length; i++) {
+    if (winnerData[i].attempts == winnerData[i - 1].attempts) {
+        temp.push(winnerData[i]);
+    } else {
+        newWinners.push(temp);
+        temp = [];
+        temp.push(winnerData[i]);
     }
-    // newWinners.push(temp);
-    temp.push(winnerData[i]);
-    return newWinners;
 }
+temp.push(winnerData[i]);
+
+console.log(newWinners);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function newRankings(winnerData, newWinner) {
+
+//     const newWinners = [];
+//     let temp = [];
+//     winnerData = winnerData.flat();
+//     winnerData.push(newWinner);
+//     winnerData.sort((a, b) => a.attempts - b.attempts);
+
+//     temp[0] = winnerData[0];
+        
+//     localStorage.getItem(username)
+    
+//     for (let i = 1; i < winnerData.length; i++) {
+//         if (winnerData[i].attempts === winnerData[i - 1].attempts) {
+//             temp.push(winnerData[i]);
+//         } else {
+//             newWinners.push(temp);
+//             temp = [];
+//             temp.push(winnerData[i]);
+//         }
+//     }
+//     // newWinners.push(temp);
+//     temp.push(winnerData[i]);
+//     return newWinners;
+// }
 
 
 
